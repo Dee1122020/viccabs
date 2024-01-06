fe1eca2be216e0572124a5040b1dfb2ad0cd7e10
sarfaraj shahjahan <sarfaraj.shahjahan@gmail.com>
2024-01-07 00:23:27 +1100

    Created NavBarMain and NavBarMobile components, and implented hamburger icon for small screen

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,38 +1,14 @@
 import React from 'react'
-import Link from 'next/link'
-import HamburgerButton from './HamburgerButton'
+import NavBarMain from './NavBarMain'
+import NavBarMobile from './NavBarMobile'
 
-function NavBar() {
+function NavBar2() {
   return (
-    <nav className='h-20 flex flex-row justify-between items-center px-6 py-1'>
-        <div className='text-3xl font-semibold ml-8'>
-            <h1><Link href="/" className='link cursor-pointer hover:opacity-60' >Vic Cabs</Link></h1>
-        </div>
-
-        <HamburgerButton />
-        
-        <div className='mr-6'>
-            <ul className='text-2xl font-medium flex flex-row gap-6'>
-                <li>
-                    <Link href="/" className='link cursor-pointer hover:opacity-40' >Home</Link>
-                </li>
-                <li>
-                    <Link href="/pages/about" className='link cursor-pointer hover:opacity-40' >About Us</Link>
-                </li>
-                <li>
-                    <Link href="/pages/services" className='link cursor-pointer hover:opacity-40' >Services</Link>
-                </li>
-                <li>
-                    <Link href="/pages/whyChoose" className='link cursor-pointer hover:opacity-40' >Why Choose Us</Link>
-                </li>
-                <li>
-                    <Link href="/pages/contact" className='link cursor-pointer hover:opacity-40' >Contact Us</Link>
-                </li>
-            </ul>
-        </div>
-
-    </nav>
+    <div>
+    <NavBarMain />
+    <NavBarMobile />
+    </div>
   )
 }
 
-export default NavBar
+export default NavBar2
---
+++ components/NavBarMobile.tsx
@@ -0,0 +1,38 @@
+import Link from 'next/link'
+import React from 'react'
+
+function NavBarMobile() {
+  return (
+    <section
+        id="mobile-menu"
+        className="absolute top-68 bg-black w-full text-5xl flex-col justify-content-center origin-top animate-open-menu hidden"
+      >
+        
+        <nav
+          className="flex flex-col min-h-screen items-center py-8"
+          aria-label="mobile"
+        >
+          <Link href="/" className="w-full text-center py-6 hover:opacity-40"
+            >Home</Link>
+
+          <Link href="/pages/about" className="w-full text-center py-6 hover:opacity-40 capitalize"
+            >about us</Link>
+
+          <Link href="/pages/services" className="w-full text-center py-6 hover:opacity-40 capitalize"
+            >services</Link>
+
+          <Link
+            href="/pages/whyChoose"
+            className="w-full text-center py-6 hover:opacity-40"
+            >Why Choose Us</Link>
+
+          <Link href="/pages/contact" className="w-full text-center py-6 hover:opacity-40"
+            >Contact Us</Link>
+
+        </nav>
+
+      </section>
+  )
+}
+
+export default NavBarMobile
