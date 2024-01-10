6f390d66c2bdd2a73f7a6a4ccd32633ad14bb503
sarfaraj shahjahan <sarfaraj.shahjahan@gmail.com>
2024-01-10 16:26:46 +1100

    Fixed why choose us page and added pictures

--- app/pages/whyChoose/page.tsx
+++ app/pages/whyChoose/page.tsx
@@ -2,108 +2,123 @@ import React from 'react'
 
 function WhyChoose() {
   return (
-    <div className='bg-black text-gray-400 p-5'>
+    <div className='bg-black text-gray-400 flex flex-col px-10 py-3 md:px-0 md:py-0'>
         <section>
-            <h2 className='text-4xl font-bold text-center sm:text-5xl mb-6 py-6'>Why Choose Victoria Cabs</h2>
+            <h2 className='text-3xl font-semibold text-center mt-6 md:mt-8 py-2 md:text-4xl md:font-semibold md:mb-3 md:py-3'>Why Choose Victoria Cabs</h2>
         </section>
 
-        <section className='mb-10'>
-            <div className='flex flex-col px-10'>
+        <div>
+            <section className='mt-6 md:mt-8'>
 
-                <h3 className='text-3xl font-semibold py-10 text-center'>Exquisite Fleet</h3>
+                <h3 className='text-2xl md:text-3xl mb-3 text-center font-semibold'>Exquisite Fleet</h3>
 
-                <div className='flex flex-row gap-40 items-center mt-10 mb-10'>
-                    <div className='w-1/3 items-center px-10'>
-                        <p className='text-1xl text-pretty sm:text-2xl'>
+                <div className='flex flex-col-reverse gap-3 p-3 md:gap-5 md:p-5 items-center mb-10 md:mb-16'>
+                    <div className='md:w-5/6'>
+                        <p className='text-xl font-medium md:text-2xl m-2 px-1 md:m-4 text-justify'>
                             Our collection of top-of-the-range vehicles caters to your every need, whether it's a corporate event, airport transfer, or a special occasion.
                         </p>
                     </div>
 
-                    <div className='w-1/2 items-center border-none rounded-sm px-2'>
-                        <img src="/callcenter.jpg" alt="Call Center" />
+                    <div className='w-5/6 md:w-3/4 p-3 md:p-6'>
+                        <img src="/luxury car2.jpg" alt="Luxury Car" />
                     </div>
 
                 </div>
+            </section>
 
-                <h3 className='text-3xl font-semibold py-10 text-center capitalize mt-24'>Professional drivers</h3>
+            <section>
 
-                <div className='flex flex-row gap-40 items-center mt-10 mb-10'>
-                    <div className='w-1/3 items-center px-10'>
-                        <p className='text-1xl text-pretty sm:text-2xl'>
+                <h3 className='text-2xl md:text-3xl mb-3 text-center font-semibold capitalize'>Professional drivers</h3>
+
+                <div className='flex flex-col-reverse gap-3 p-3 md:gap-5 md:p-5 items-center mb-10 md:mb-16'>
+                    <div className='md:w-5/6'>
+                        <p className='text-xl font-medium md:text-2xl m-2 px-1 md:m-4 text-justify'>
                         Our chauffeurs and taxi drivers are not just drivers; they are trained professionals dedicated to providing a service that exceeds expectations.
                         </p>
                     </div>
 
-                    <div className='w-1/2 items-center border-none rounded-sm px-2'>
-                        <img src="/callcenter.jpg" alt="Call Center" />
+                    <div className='w-5/6 md:w-3/4 p-3 md:p-6'>
+                        <img src="/chauffeur3.jpg" alt="Proffessional Driver" />
                     </div>
 
                 </div>
+            </section>
 
-                <h3 className='text-3xl font-semibold py-10 text-center mt-24'>Tailored Experiences</h3>
+            <section>
 
-                <div className='flex flex-row gap-40 items-center mt-10 mb-10'>
-                    <div className='w-1/3 items-center px-10'>
-                        <p className='text-1xl text-pretty sm:text-2xl'>
+                <h3 className='text-2xl md:text-3xl mb-3 text-center font-semibold capitalize'>Tailored Experiences</h3>
+
+                <div className='flex flex-col-reverse gap-3 p-3 md:gap-5 md:p-5 items-center mb-10 md:mb-16'>
+                    <div className='md:w-5/6'>
+                        <p className='text-xl font-medium md:text-2xl m-2 px-1 md:m-4 text-justify'>
                         We understand that every journey is unique. Our team is committed to tailoring our services to meet your specific requirements, ensuring a personalized experience.
                         </p>
                     </div>
 
-                    <div className='w-1/2 items-center border-none rounded-sm px-2'>
-                        <img src="/callcenter.jpg" alt="Call Center" />
+                    <div className='w-5/6 md:w-3/4 p-3 md:p-6'>
+                        <img src="/female driver.jpg" alt="Call Center" />
                     </div>
 
                 </div>
+            </section>
 
-                <h3 className='text-3xl font-semibold py-10 text-center mt-24'>Reliability</h3>
+            <section>
 
-                <div className='flex flex-row gap-40 items-center mt-10 mb-10'>
-                    <div className='w-1/3 items-center px-10'>
-                        <p className='text-1xl text-pretty sm:text-2xl'>
+                <h3 className='text-2xl md:text-3xl mb-3 text-center font-semibold capitalize'>Reliability</h3>
+
+                <div className='flex flex-col-reverse gap-3 p-3 md:gap-5 md:p-5 items-center mb-10 md:mb-16'>
+                    <div className='md:w-5/6'>
+                        <p className='text-xl font-medium md:text-2xl m-2 px-1 md:m-4 text-justify'>
                         Punctuality and reliability are at the core of our service. When you choose Victoria Cabs, you can trust us to get you to your destination on time, every time.
                         </p>
                     </div>
 
-                    <div className='w-1/2 items-center border-none rounded-sm px-2'>
-                        <img src="/callcenter.jpg" alt="Call Center" />
+                    <div className='w-5/6 md:w-3/4 p-3 md:p-6'>
+                        <img src="/woman in the car.jpg" alt="Reliable Service" />
                     </div>
 
                 </div>
+            </section>
 
-                <h3 className='text-3xl font-semibold py-10 text-center mt-24'>Airport Excellence</h3>
+            <section>
 
-                <div className='flex flex-row gap-40 items-center mt-10 mb-10'>
-                    <div className='w-1/3 items-center px-10'>
-                        <p className='text-1xl text-pretty sm:text-2xl'>
+                <h3 className='text-2xl md:text-3xl mb-3 text-center font-semibold capitalize'>Airport Excellence</h3>
+
+                <div className='flex flex-col-reverse gap-3 p-3 md:gap-5 md:p-5 items-center mb-10 md:mb-16'>
+                    <div className='md:w-5/6'>
+                        <p className='text-xl font-medium md:text-2xl m-2 px-1 md:m-4 text-justify'>
                         Trust us for reliable and stress-free airport transfers, ensuring you reach your destination on time.
                         </p>
                     </div>
 
-                    <div className='w-1/2 items-center border-none rounded-sm px-2'>
-                        <img src="/callcenter.jpg" alt="Call Center" />
+                    <div className='w-5/6 md:w-3/4 p-3 md:p-6'>
+                        <img src="/airport transfer2.jpg" alt="Airport Service" />
                     </div>
 
                 </div>
+            </section>
 
-                <h3 className='text-3xl font-semibold py-10 text-center mt-24'>Family-Friendly</h3>
+            <section>
 
-                <div className='flex flex-row gap-40 items-center mt-10 mb-10'>
-                    <div className='w-1/3 items-center px-10'>
-                        <p className='text-1xl text-pretty sm:text-2xl'>
+                <h3 className='text-2xl md:text-3xl mb-3 text-center font-semibold capitalize'>Family-Friendly</h3>
+
+                <div className='flex flex-col-reverse gap-3 p-3 md:gap-5 md:p-5 items-center mb-10 md:mb-16'>
+                    <div className='md:w-5/6'>
+                        <p className='text-xl font-medium md:text-2xl m-2 px-1 md:m-4 text-justify'>
                         Our bigger SUVs cater to the needs of families, providing spacious and comfortable transportation.
                         </p>
                     </div>
 
-                    <div className='w-1/2 items-center border-none rounded-sm px-2'>
-                        <img src="/callcenter.jpg" alt="Call Center" />
+                    <div className='w-5/6 md:w-3/4 p-3 md:p-6'>
+                        <img src="/family friendly.jpg" alt="Family Friendly Service" />
                     </div>
 
                 </div>
 
 
-            </div>
+            </section>
             
-        </section>
+        </div>
     </div>
   )
 }
