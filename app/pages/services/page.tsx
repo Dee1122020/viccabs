d35d6def0db67ac2e749ae5428d6979097839fa0
sarfaraj shahjahan <sarfaraj.shahjahan@gmail.com>
2024-01-10 15:27:21 +1100

    fixed styling of services page and put pictures in the public folder

--- app/pages/services/page.tsx
+++ app/pages/services/page.tsx
@@ -2,67 +2,67 @@ import React from 'react'
 
 function Services() {
   return (
-    <div className='bg-black text-gray-400 p-5'>
-      <div className='flex flex-col'>
+    <div className='bg-black text-gray-400 flex flex-col px-10 py-3 md:px-0 md:py-0'>
+      <div>
         <div>
-          <h2 className='text-4xl font-bold text-center sm:text-5xl mb-6 py-6'>Our Services</h2>
+          <h2 className='text-3xl font-semibold text-center mt-6 md:mt-8 py-2 md:text-4xl md:font-semibold md:mb-3 md:py-3'>Our Services</h2>
         </div>
         <div>
-          <section>
-            <h3 className="text-3xl font-semibold text-center">Premium Taxi Service
+          <section className='mt-6 md:mt-8'>
+            <h3 className="text-2xl md:text-3xl mb-3 text-center font-semibold">Premium Taxi Service
             </h3>
-            <div className='flex flex-col-reverse gap-5 p-5 items-center mb-20'>
-              <div className='w-5/6'>
-                <p className='text-2xl text-justify'>
+            <div className='flex flex-col-reverse gap-3 p-3 md:gap-5 md:p-5 items-center mb-10 md:mb-16'>
+              <div className='md:w-5/6'>
+                <p className='text-xl font-medium md:text-2xl m-2 px-1 md:m-4 text-justify'>
                 Victoria Cabs takes pride in offering a premium taxi service that goes beyond the ordinary. Our fleet of meticulously maintained vehicles ensures a comfortable and stylish journey for every passenger. Whether you're heading to a business meeting or exploring the city, our premium taxi service combines reliability with luxury.
                 </p>
               </div>
-              <div className='w-5/6 p-10'>
+              <div className='w-5/6 md:w-3/4 p-3 md:p-6'>
                 <img src="/chauffeur.jpg" alt="Chauffeur" />
               </div>
             </div>
           </section>
           
           <section>
-            <h3 className="text-3xl font-semibold text-center">Chauffeur Service
+            <h3 className="text-3xl font-semibold text-center mt-6 md:mt-8 py-2 md:text-4xl md:font-semibold md:mb-3 md:py-3">Chauffeur Service
             </h3>
-            <div className='flex flex-col-reverse gap-5 p-5 items-center mb-20'>
-              <div className='w-5/6'>
-                <p className='text-2xl text-justify'>
+            <div className='flex flex-col-reverse gap-3 p-3 md:gap-5 md:p-5 items-center mb-10 md:mb-16'>
+              <div className='md:w-5/6'>
+                <p className='text-xl font-medium md:text-2xl m-2 px-1 md:m-4 text-justify'>
                 Indulge in the ultimate chauffeur experience with Victoria Cabs. Our professional chauffeurs are dedicated to providing a service that exceeds expectations. From corporate events to special occasions, our chauffeur service is designed to add a touch of sophistication to your every journey.
                 </p>
               </div>
-              <div className='w-5/6 p-10'>
+              <div className='w-5/6 md:w-3/4 p-3 md:p-6'>
                 <img src="/chauffeur.jpg" alt="Chauffeur" />
               </div>
             </div>
           </section>
 
           <section>
-            <h3 className="text-3xl font-semibold text-center">Airport Transfer
+            <h3 className="text-3xl font-semibold text-center mt-6 md:mt-8 py-2 md:text-4xl md:font-semibold md:mb-3 md:py-3">Airport Transfer
             </h3>
-            <div className='flex flex-col-reverse gap-5 p-5 items-center mb-20'>
-              <div className='w-5/6'>
-                <p className='text-2xl text-justify'>
+            <div className='flex flex-col-reverse gap-3 p-3 md:gap-5 md:p-5 items-center mb-10 md:mb-16'>
+              <div className='md:w-5/6'>
+                <p className='text-xl font-medium md:text-2xl m-2 px-1 md:m-4 text-justify'>
                 Start and end your travels with seamless airport transfers by Victoria Cabs. Our prompt and reliable service ensures that you reach your destination on time, stress-free. Experience the convenience of door-to-door transfers with our expert drivers and premium vehicles.
                 </p>
               </div>
-              <div className='w-5/6 p-10'>
+              <div className='w-5/6 md:w-3/4 p-3 md:p-6'>
                 <img src="/chauffeur.jpg" alt="Chauffeur" />
               </div>
             </div>
           </section>
 
           <section>
-            <h3 className="text-3xl font-semibold text-center">Family-Friendly SUVs
+            <h3 className="text-3xl font-semibold text-center mt-6 md:mt-8 py-2 md:text-4xl md:font-semibold md:mb-3 md:py-3">Family-Friendly SUVs
             </h3>
-            <div className='flex flex-col-reverse gap-5 p-5 items-center mb-20'>
-              <div className='w-5/6'>
-                <p className='text-2xl text-justify'>
+            <div className='flex flex-col-reverse gap-3 p-3 md:gap-5 md:p-5 items-center mb-10 md:mb-16'>
+              <div className='md:w-5/6'>
+                <p className='text-xl font-medium md:text-2xl m-2 px-1 md:m-4 text-justify'>
                 For your family's transportation needs, Victoria Cabs offers spacious and comfortable SUVs. Whether you're planning a day out or need transportation for a larger group, our bigger SUVs are equipped to provide a safe and enjoyable journey for the whole family.
                 </p>
               </div>
-              <div className='w-5/6 p-10'>
+              <div className='w-5/6 md:w-3/4 p-3 md:p-6'>
                 <img src="/chauffeur.jpg" alt="Chauffeur" />
               </div>
             </div>
